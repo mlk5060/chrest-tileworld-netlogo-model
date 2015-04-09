b00abccc79d3979b9e2c4e493810253a29309ca8
martyn <martynlloydkelly@gmail.com>
2015-04-09 17:08:36 +0100

    Respecified scenarios

--- input-output/SpringerExtension/CreateExtensionInputOutputDirectories.sh
+++ input-output/SpringerExtension/CreateExtensionInputOutputDirectories.sh
@@ -1,327 +1,53 @@
 #!/bin/bash       
 TOP_LEVEL_DIRECTORY=$(pwd)
 SCENARIO_NUMBER=1
-while [ $SCENARIO_NUMBER -le 972 ]; do
+while [ $SCENARIO_NUMBER -le 270 ]; do
 	mkdir "Scenario$SCENARIO_NUMBER"
 	cd "Scenario$SCENARIO_NUMBER"
-	if [ $SCENARIO_NUMBER -le 324 ]; then
+	
+	# Determine complexity
+	INTEGER=$((SCENARIO_NUMBER/90))
+	FRACTION=$((SCENARIO_NUMBER%90))
+	if [ \( $INTEGER -eq 0 -a $FRACTION -gt 0 \) -o \( $INTEGER -eq 1 -a $FRACTION -eq 0 \) ]; then
 		ENV_COMPLEXITY=1
-		if [ $SCENARIO_NUMBER -le 108 ]; then
-			NUM_PLAYERS=2
-			if [ $SCENARIO_NUMBER -le 54 ]; then
-				AGENT_TYPE=2
-				if [ $SCENARIO_NUMBER -le 9 ]; then
-					EPISODIC_MEMORY_SIZE=5
-				elif [ $SCENARIO_NUMBER -le 18 ]; then
-					EPISODIC_MEMORY_SIZE=6
-				elif [ $SCENARIO_NUMBER -le 27 ]; then
-					EPISODIC_MEMORY_SIZE=7
-				elif [ $SCENARIO_NUMBER -le 36 ]; then
-					EPISODIC_MEMORY_SIZE=8
-				elif [ $SCENARIO_NUMBER -le 45 ]; then
-					EPISODIC_MEMORY_SIZE=9
-				else
-					EPISODIC_MEMORY_SIZE=10
-				fi
-			else
-				AGENT_TYPE=3
-				if [ $SCENARIO_NUMBER -le 63 ]; then
-					EPISODIC_MEMORY_SIZE=5
-				elif [ $SCENARIO_NUMBER -le 72 ]; then
-					EPISODIC_MEMORY_SIZE=6
-				elif [ $SCENARIO_NUMBER -le 81 ]; then
-					EPISODIC_MEMORY_SIZE=7
-				elif [ $SCENARIO_NUMBER -le 90 ]; then
-					EPISODIC_MEMORY_SIZE=8
-				elif [ $SCENARIO_NUMBER -le 99 ]; then
-					EPISODIC_MEMORY_SIZE=9
-				else
-					EPISODIC_MEMORY_SIZE=10
-				fi
-			fi
-		fi
-		if [ $SCENARIO_NUMBER -gt 108 -a $SCENARIO_NUMBER -le 216 ]; then
-			NUM_PLAYERS=4
-			if [ $SCENARIO_NUMBER -le 162 ]; then
-				AGENT_TYPE=2
-				if [ $SCENARIO_NUMBER -le 117 ]; then
-					EPISODIC_MEMORY_SIZE=5
-				elif [ $SCENARIO_NUMBER -le 126 ]; then
-					EPISODIC_MEMORY_SIZE=6
-				elif [ $SCENARIO_NUMBER -le 135 ]; then
-					EPISODIC_MEMORY_SIZE=7
-				elif [ $SCENARIO_NUMBER -le 144 ]; then
-					EPISODIC_MEMORY_SIZE=8
-				elif [ $SCENARIO_NUMBER -le 153 ]; then
-					EPISODIC_MEMORY_SIZE=9
-				else
-					EPISODIC_MEMORY_SIZE=10
-				fi
-			else
-				AGENT_TYPE=3
-				if [ $SCENARIO_NUMBER -le 171 ]; then
-					EPISODIC_MEMORY_SIZE=5
-				elif [ $SCENARIO_NUMBER -le 180 ]; then
-					EPISODIC_MEMORY_SIZE=6
-				elif [ $SCENARIO_NUMBER -le 189 ]; then
-					EPISODIC_MEMORY_SIZE=7
-				elif [ $SCENARIO_NUMBER -le 198 ]; then
-					EPISODIC_MEMORY_SIZE=8
-				elif [ $SCENARIO_NUMBER -le 207 ]; then
-					EPISODIC_MEMORY_SIZE=9
-				else
-					EPISODIC_MEMORY_SIZE=10
-				fi
-			fi
-		fi
-		if [ $SCENARIO_NUMBER -gt 216 ]; then
-			NUM_PLAYERS=8
-			if [ $SCENARIO_NUMBER -le 270 ]; then
-				AGENT_TYPE=2
-				if [ $SCENARIO_NUMBER -le 225 ]; then
-					EPISODIC_MEMORY_SIZE=5
-				elif [ $SCENARIO_NUMBER -le 234 ]; then
-					EPISODIC_MEMORY_SIZE=6
-				elif [ $SCENARIO_NUMBER -le 243 ]; then
-					EPISODIC_MEMORY_SIZE=7
-				elif [ $SCENARIO_NUMBER -le 252 ]; then
-					EPISODIC_MEMORY_SIZE=8
-				elif [ $SCENARIO_NUMBER -le 261 ]; then
-					EPISODIC_MEMORY_SIZE=9
-				else
-					EPISODIC_MEMORY_SIZE=10
-				fi
-			else
-				AGENT_TYPE=3
-				if [ $SCENARIO_NUMBER -le 279 ]; then
-					EPISODIC_MEMORY_SIZE=5
-				elif [ $SCENARIO_NUMBER -le 288 ]; then
-					EPISODIC_MEMORY_SIZE=6
-				elif [ $SCENARIO_NUMBER -le 297 ]; then
-					EPISODIC_MEMORY_SIZE=7
-				elif [ $SCENARIO_NUMBER -le 306 ]; then
-					EPISODIC_MEMORY_SIZE=8
-				elif [ $SCENARIO_NUMBER -le 315 ]; then
-					EPISODIC_MEMORY_SIZE=9
-				else
-					EPISODIC_MEMORY_SIZE=10
-				fi
-			fi
-		fi
-	fi
-	if [ $SCENARIO_NUMBER -gt 324 -a $SCENARIO_NUMBER -le 648 ]; then
+		NUM_PLAYERS=2
+		NUMERATOR=90
+	elif [ \( $INTEGER -eq 1 -a $FRACTION -gt 0 \) -o \( $INTEGER -eq 2 -a $FRACTION -eq 0 \) ]; then
 		ENV_COMPLEXITY=2
-		if [ $SCENARIO_NUMBER -le 432 ]; then
-			NUM_PLAYERS=2
-			if [ $SCENARIO_NUMBER -le 378 ]; then
-				AGENT_TYPE=2
-				if [ $SCENARIO_NUMBER -le 333 ]; then
-					EPISODIC_MEMORY_SIZE=5
-				elif [ $SCENARIO_NUMBER -le 342 ]; then
-					EPISODIC_MEMORY_SIZE=6
-				elif [ $SCENARIO_NUMBER -le 351 ]; then
-					EPISODIC_MEMORY_SIZE=7
-				elif [ $SCENARIO_NUMBER -le 360 ]; then
-					EPISODIC_MEMORY_SIZE=8
-				elif [ $SCENARIO_NUMBER -le 369 ]; then
-					EPISODIC_MEMORY_SIZE=9
-				else
-					EPISODIC_MEMORY_SIZE=10
-				fi
-			else
-				AGENT_TYPE=3
-				if [ $SCENARIO_NUMBER -le 387 ]; then
-					EPISODIC_MEMORY_SIZE=5
-				elif [ $SCENARIO_NUMBER -le 396 ]; then
-					EPISODIC_MEMORY_SIZE=6
-				elif [ $SCENARIO_NUMBER -le 405 ]; then
-					EPISODIC_MEMORY_SIZE=7
-				elif [ $SCENARIO_NUMBER -le 414 ]; then
-					EPISODIC_MEMORY_SIZE=8
-				elif [ $SCENARIO_NUMBER -le 423 ]; then
-					EPISODIC_MEMORY_SIZE=9
-				else
-					EPISODIC_MEMORY_SIZE=10
-				fi
-			fi
-		fi
-		if [ $SCENARIO_NUMBER -gt 432 -a $SCENARIO_NUMBER -le 540 ]; then
-			NUM_PLAYERS=4
-			if [ $SCENARIO_NUMBER -le 486 ]; then
-				AGENT_TYPE=2
-				if [ $SCENARIO_NUMBER -le 441 ]; then
-					EPISODIC_MEMORY_SIZE=5
-				elif [ $SCENARIO_NUMBER -le 450 ]; then
-					EPISODIC_MEMORY_SIZE=6
-				elif [ $SCENARIO_NUMBER -le 459 ]; then
-					EPISODIC_MEMORY_SIZE=7
-				elif [ $SCENARIO_NUMBER -le 468 ]; then
-					EPISODIC_MEMORY_SIZE=8
-				elif [ $SCENARIO_NUMBER -le 477 ]; then
-					EPISODIC_MEMORY_SIZE=9
-				else
-					EPISODIC_MEMORY_SIZE=10
-				fi
-			else
-				AGENT_TYPE=3
-				if [ $SCENARIO_NUMBER -le 495 ]; then
-					EPISODIC_MEMORY_SIZE=5
-				elif [ $SCENARIO_NUMBER -le 504 ]; then
-					EPISODIC_MEMORY_SIZE=6
-				elif [ $SCENARIO_NUMBER -le 513 ]; then
-					EPISODIC_MEMORY_SIZE=7
-				elif [ $SCENARIO_NUMBER -le 522 ]; then
-					EPISODIC_MEMORY_SIZE=8
-				elif [ $SCENARIO_NUMBER -le 531 ]; then
-					EPISODIC_MEMORY_SIZE=9
-				else
-					EPISODIC_MEMORY_SIZE=10
-				fi
-			fi
-		fi
-		if [ $SCENARIO_NUMBER -gt 540 ]; then
-			NUM_PLAYERS=8
-			if [ $SCENARIO_NUMBER -le 594 ]; then
-				AGENT_TYPE=2
-				if [ $SCENARIO_NUMBER -le 549 ]; then
-					EPISODIC_MEMORY_SIZE=5
-				elif [ $SCENARIO_NUMBER -le 558 ]; then
-					EPISODIC_MEMORY_SIZE=6
-				elif [ $SCENARIO_NUMBER -le 567 ]; then
-					EPISODIC_MEMORY_SIZE=7
-				elif [ $SCENARIO_NUMBER -le 576 ]; then
-					EPISODIC_MEMORY_SIZE=8
-				elif [ $SCENARIO_NUMBER -le 585 ]; then
-					EPISODIC_MEMORY_SIZE=9
-				else
-					EPISODIC_MEMORY_SIZE=10
-				fi
-			else
-				AGENT_TYPE=3
-				if [ $SCENARIO_NUMBER -le 603 ]; then
-					EPISODIC_MEMORY_SIZE=5
-				elif [ $SCENARIO_NUMBER -le 612 ]; then
-					EPISODIC_MEMORY_SIZE=6
-				elif [ $SCENARIO_NUMBER -le 621 ]; then
-					EPISODIC_MEMORY_SIZE=7
-				elif [ $SCENARIO_NUMBER -le 630 ]; then
-					EPISODIC_MEMORY_SIZE=8
-				elif [ $SCENARIO_NUMBER -le 639 ]; then
-					EPISODIC_MEMORY_SIZE=9
-				else
-					EPISODIC_MEMORY_SIZE=10
-				fi
-			fi
-		fi
-	fi
-	if [ $SCENARIO_NUMBER -gt 648 ]; then
+		NUM_PLAYERS=4
+		NUMERATOR=180
+	else
 		ENV_COMPLEXITY=3
-		if [ $SCENARIO_NUMBER -le 756 ]; then
-			NUM_PLAYERS=2
-			if [ $SCENARIO_NUMBER -le 702 ]; then
-				AGENT_TYPE=2
-				if [ $SCENARIO_NUMBER -le 657 ]; then
-					EPISODIC_MEMORY_SIZE=5
-				elif [ $SCENARIO_NUMBER -le 666 ]; then
-					EPISODIC_MEMORY_SIZE=6
-				elif [ $SCENARIO_NUMBER -le 675 ]; then
-					EPISODIC_MEMORY_SIZE=7
-				elif [ $SCENARIO_NUMBER -le 684 ]; then
-					EPISODIC_MEMORY_SIZE=8
-				elif [ $SCENARIO_NUMBER -le 693 ]; then
-					EPISODIC_MEMORY_SIZE=9
-				else
-					EPISODIC_MEMORY_SIZE=10
-				fi
-			else
-				AGENT_TYPE=3
-				if [ $SCENARIO_NUMBER -le 711 ]; then
-					EPISODIC_MEMORY_SIZE=5
-				elif [ $SCENARIO_NUMBER -le 720 ]; then
-					EPISODIC_MEMORY_SIZE=6
-				elif [ $SCENARIO_NUMBER -le 729 ]; then
-					EPISODIC_MEMORY_SIZE=7
-				elif [ $SCENARIO_NUMBER -le 738 ]; then
-					EPISODIC_MEMORY_SIZE=8
-				elif [ $SCENARIO_NUMBER -le 747 ]; then
-					EPISODIC_MEMORY_SIZE=9
-				else
-					EPISODIC_MEMORY_SIZE=10
-				fi
-			fi
-		fi
-		if [ $SCENARIO_NUMBER -gt 756 -a $SCENARIO_NUMBER -le 864 ]; then
-			NUM_PLAYERS=4
-			if [ $SCENARIO_NUMBER -le 810 ]; then
-				AGENT_TYPE=2
-				if [ $SCENARIO_NUMBER -le 765 ]; then
-					EPISODIC_MEMORY_SIZE=5
-				elif [ $SCENARIO_NUMBER -le 774 ]; then
-					EPISODIC_MEMORY_SIZE=6
-				elif [ $SCENARIO_NUMBER -le 783 ]; then
-					EPISODIC_MEMORY_SIZE=7
-				elif [ $SCENARIO_NUMBER -le 792 ]; then
-					EPISODIC_MEMORY_SIZE=8
-				elif [ $SCENARIO_NUMBER -le 801 ]; then
-					EPISODIC_MEMORY_SIZE=9
-				else
-					EPISODIC_MEMORY_SIZE=10
-				fi
-			else
-				AGENT_TYPE=3
-				if [ $SCENARIO_NUMBER -le 819 ]; then
-					EPISODIC_MEMORY_SIZE=5
-				elif [ $SCENARIO_NUMBER -le 828 ]; then
-					EPISODIC_MEMORY_SIZE=6
-				elif [ $SCENARIO_NUMBER -le 837 ]; then
-					EPISODIC_MEMORY_SIZE=7
-				elif [ $SCENARIO_NUMBER -le 846 ]; then
-					EPISODIC_MEMORY_SIZE=8
-				elif [ $SCENARIO_NUMBER -le 855 ]; then
-					EPISODIC_MEMORY_SIZE=9
-				else
-					EPISODIC_MEMORY_SIZE=10
-				fi
-			fi
-		fi
-		if [ $SCENARIO_NUMBER -gt 864 ]; then
-			NUM_PLAYERS=8
-			if [ $SCENARIO_NUMBER -le 918 ]; then
-				AGENT_TYPE=2
-				if [ $SCENARIO_NUMBER -le 873 ]; then
-					EPISODIC_MEMORY_SIZE=5
-				elif [ $SCENARIO_NUMBER -le 882 ]; then
-					EPISODIC_MEMORY_SIZE=6
-				elif [ $SCENARIO_NUMBER -le 891 ]; then
-					EPISODIC_MEMORY_SIZE=7
-				elif [ $SCENARIO_NUMBER -le 900 ]; then
-					EPISODIC_MEMORY_SIZE=8
-				elif [ $SCENARIO_NUMBER -le 909 ]; then
-					EPISODIC_MEMORY_SIZE=9
-				else
-					EPISODIC_MEMORY_SIZE=10
-				fi
-			else
-				AGENT_TYPE=3
-				if [ $SCENARIO_NUMBER -le 927 ]; then
-					EPISODIC_MEMORY_SIZE=5
-				elif [ $SCENARIO_NUMBER -le 936 ]; then
-					EPISODIC_MEMORY_SIZE=6
-				elif [ $SCENARIO_NUMBER -le 945 ]; then
-					EPISODIC_MEMORY_SIZE=7
-				elif [ $SCENARIO_NUMBER -le 954 ]; then
-					EPISODIC_MEMORY_SIZE=8
-				elif [ $SCENARIO_NUMBER -le 963 ]; then
-					EPISODIC_MEMORY_SIZE=9
-				else
-					EPISODIC_MEMORY_SIZE=10
-				fi
-			fi
-		fi
+		NUM_PLAYERS=8
+		NUMERATOR=270
 	fi
 
-	MOD=$(($SCENARIO_NUMBER%9))
-	case $MOD in
+	#Determine agent type
+	if [ $SCENARIO_NUMBER -ge $((NUMERATOR-90)) -a $SCENARIO_NUMBER -le $(((NUMERATOR-90)+45)) ]; then
+		AGENT_TYPE=2
+		AGENT_TYPE_START=$((NUMERATOR-90))
+	else
+		AGENT_TYPE=3
+		AGENT_TYPE_START=$((NUMERATOR-45))
+	fi
+
+	#Determine episodic memory size
+	if [ $SCENARIO_NUMBER -ge $AGENT_TYPE_START -a $SCENARIO_NUMBER -le $(($AGENT_TYPE_START+9)) ]; then
+		EPISODIC_MEMORY_SIZE=6
+	elif [ $SCENARIO_NUMBER -ge $AGENT_TYPE_START -a $SCENARIO_NUMBER -le $(($AGENT_TYPE_START+18)) ]; then
+		EPISODIC_MEMORY_SIZE=8
+	elif [ $SCENARIO_NUMBER -ge $AGENT_TYPE_START -a $SCENARIO_NUMBER -le $(($AGENT_TYPE_START+27)) ]; then
+		EPISODIC_MEMORY_SIZE=10
+	elif [ $SCENARIO_NUMBER -ge $AGENT_TYPE_START -a $SCENARIO_NUMBER -le $(($AGENT_TYPE_START+36)) ]; then
+		EPISODIC_MEMORY_SIZE=12
+	elif [ $SCENARIO_NUMBER -ge $AGENT_TYPE_START -a $SCENARIO_NUMBER -le $(($AGENT_TYPE_START+45)) ]; then
+		EPISODIC_MEMORY_SIZE=14
+	fi
+
+
+	#Determine discount rate
+	FRACTION=$((SCENARIO_NUMBER%9))
+	case $FRACTION in
 		1)
 			DISCOUNT_RATE=0.1;;
 		2)
